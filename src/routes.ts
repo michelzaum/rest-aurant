4a5166f63da34f052ee651e192f79f913a509baa
Michel Oliveira <michel.oliveira@vizir.com.br>
2022-01-19 20:52:30 -0300

    Changing the export in routes file

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import FoodController from './controllers/FoodController';
 
-export const router = Router();
+const router = Router();
 
 router.get('/foods', FoodController.listAll);
 router.get('/foods/:id', FoodController.findFood);
+
+export default router;
