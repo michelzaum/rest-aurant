316bb503b41045c8ff8f7940912d018c07fc421d
Michel Oliveira <michel.oliveira@vizir.com.br>
2022-01-22 16:55:41 -0300

    Adding image in mock categories

--- back-end/src/mock/caterory_mock.js
+++ back-end/src/mock/caterory_mock.js
@@ -1,34 +1,44 @@
 const categories = [
   {
     category_id: 1,
-    name: "Hamburguer"
+    name: "Hamburguer",
+    image: "https://images.unsplash.com/photo-1603064752734-4c48eff53d05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1288&q=80",
   }, {
     category_id: 2,
-    name: "Vegano"
+    name: "Vegano",
+    image: "https://images.unsplash.com/photo-1519996409144-56c88c9aa612?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   }, {
     category_id: 3,
-    name: "Carne"
+    name: "Carne",
+    image: "https://images.unsplash.com/photo-1608877907149-a206d75ba011?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
   }, {
     category_id: 4,
-    name: "Frango"
+    name: "Frango",
+    image: "https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
   }, {
     category_id: 5,
-    name: "Massas"
+    name: "Massas",
+    image: "https://images.unsplash.com/photo-1597393353415-b3730f3719fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   }, {
     category_id: 6,
-    name: "Fitness"
+    name: "Fitness",
+    image: "https://images.unsplash.com/photo-1614648692330-eb129aeb6880?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
   }, {
     category_id: 7,
-    name: "Peixes"
+    name: "Peixes",
+    image: "https://images.unsplash.com/photo-1510130387422-82bed34b37e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80",
   }, {
     category_id: 8,
-    name: "Doces"
+    name: "Doces",
+    image: "https://images.unsplash.com/photo-1575224300306-1b8da36134ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
   }, {
     category_id: 9,
-    name: "Café da manhã"
+    name: "Café da manhã",
+    image: "https://images.unsplash.com/photo-1528699633788-424224dc89b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=733&q=80",
   }, {
     category_id: 10,
-    name: "Bebidas"
+    name: "Bebidas",
+    image: "https://images.unsplash.com/photo-1597075759290-5c29a23c8a16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   }
 ]
 
